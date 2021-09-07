6dc8abd46c41c1b3944b7c280b7f6726ade81e0a
Pratik Gorade <me@prkprime.dev>
2021-09-07 15:14:50 +0530

    api: endpoints: created routes for expense and i feel stupid
    Signed-off-by: Pratik Gorade <me@prkprime.dev>

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .endpoints import income
+from .endpoints import income, expense
 
 api_router = APIRouter()
 
 api_router.include_router(income.router, tags=["Income"])
+api_router.include_router(expense.router, tags=["Expense"])
